0caaed92c467bcecce23a4297dcf041760496a79
juli_e <julia.ero@euedge.com>
2019-01-07 17:52:21 +0100

    add circles, adjust tempo and layout

--- src/components/circle.vue
+++ src/components/circle.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <button @click="resetBeats">reset</button>
-    <vue-p5 @setup="setup"
+    <button @click="resetBeats">reset beats</button>
+    <button @click="resetCircles">reset circles</button>
+    <button @click="addCircle">add circle</button>
+    <input type="range" id="tempo" name="tempo"
+           min="30" max="180" v-model="tempo">
+    <label for="tempo">tempo</label>
+    <div>
+      <label for="three">thirds</label>
+      <input type="radio" name="layout" id="three" value="3" v-model="layout">
+      <label for="four">fourths</label>
+      <input type="radio" name="layout" id="four" value="4"  v-model="layout">
+    </div>
+    <vue-p5 ref="rounds" @setup="setup"
             @draw="draw"
             @mouseclicked="mouseclicked"
     ></vue-p5>
@@ -10,137 +21,164 @@
 </template>
 
 <script>
-import VueP5 from 'vue-p5';
-/* eslint-disable no-console*/
+  import VueP5 from 'vue-p5'
+  /* eslint-disable no-console*/
 
-export default {
-  name: 'circleCanvas',
-  data () {
-    return {
-      angles: 12,
-      angle: 0,
-      tempo: 45,
-      width: 0,
-      height: 0,
-      radius: 0,
-      center: 0,
-      dots: [],
-      dotSize: 5,
-      beats: [],
-      beatSize: 25,
-      sound: null
-    }
-  },
-  computed: {
-//    dots () {
-//      return this.angles.map(item => {
-//        const x = this.center.x - (this.radius * Math.sin(item * -1))
-//        const y = this.center.y - (this.radius * Math.cos(item * -1))
-//        return {x, y}
-//      })
-//    }
-  },
-  methods: {
-    isAreaDetected(pointer ,target) {
-      const n = 10
-      return (pointer.x <= target.x + n && pointer.x >= target.x - n
-        && pointer.y <= target.y + n && pointer.y >= target.y - n)
-    },
-    setup(sketch) {
-      sketch.createCanvas(500, 500)
-      sketch.background('#fafafa')
-      sketch.strokeWeight(6)
-      this.width = sketch.width
-      this.height = sketch.height
-      this.radius = this.width / 3
-      this.center = {
-        x: this.width / 2,
-        y: this.height / 2
-      }
-      this.createDots(sketch)
-    },
-    createDots(sketch) {
-      sketch.angleMode(sketch.DEGREES)
-
-      for (let i = 0; i < this.angles; i++) {
-        let angle = i * 360 / this.angles
-        const x = this.center.x - (this.radius * sketch.sin(angle * -1))
-        const y = this.center.y - (this.radius * sketch.cos(angle * -1))
-        this.dots.push(sketch.createVector(x, y))
+  export default {
+    name: 'circleCanvas',
+    data () {
+      return {
+        layout: 4,
+        angle: 0,
+        tempo: 60,
+        width: 0,
+        height: 0,
+        radius: 0,
+        center: 0,
+        circles: [],
+        dots: [],
+        dotSize: 5,
+        beats: [],
+        beatSize: 25,
+        sound: null,
       }
     },
-    draw(sketch) {
-      sketch.clear()
-      this.drawMainCircle(sketch)
-      this.drawFinger(sketch)
-      this.drawDots(sketch)
-      this.drawBeats(sketch)
+    computed: {
+       angles() {
+        return parseInt(this.layout, 10) * 3
+       }
     },
-    drawMainCircle(sketch) {
-      const size = this.width * 2 / 3
-      sketch.stroke('#34ebac')
-      sketch.noFill()
-      sketch.ellipse(this.width / 2, this.height / 2, size)
-    },
-    drawFinger(sketch) {
-      const end = {
-        x: this.center.x - (this.radius * sketch.sin(this.angle)),
-        y: this.center.y - (this.radius * sketch.cos(this.angle))
-      }
-      sketch.angleMode(sketch.DEGREES)
-      sketch.stroke('#6a44eb')
-      sketch.line(this.center.x, this.center.y, end.x, end.y)
-
-      this.angle -= this.tempo / 60
-
-      this.beats.forEach(beat => {
-        if (this.isAreaDetected(end, beat)) {
-          this.drawFullDot(sketch, beat, this.beatSize * 1.6, '#d93f28')
+    methods: {
+      isAreaDetected (pointer, target) {
+        const n = 10
+        return (pointer.x <= target.x + n && pointer.x >= target.x - n
+          && pointer.y <= target.y + n && pointer.y >= target.y - n)
+      },
+      setup (sketch) {
+        sketch.createCanvas(500, 500)
+        sketch.background('#fafafa')
+        sketch.strokeWeight(6)
+        this.width = sketch.width
+        this.height = sketch.height
+        this.radius = this.width / 3
+        this.circles.push(this.radius * 2)
+        this.center = {
+          x: this.width / 2,
+          y: this.height / 2,
         }
-      })
-    },
-    drawDots(sketch) {
-      this.dots.forEach(item => {
-        this.drawFullDot(sketch, item, this.dotSize, '#321fb2')
-      })
-    },
-    drawBeats(sketch) {
-      this.beats.forEach(beat => {
-        this.drawFullDot(sketch, beat, this.beatSize, '#d93f28')
-      })
-    },
-    mouseclicked(sketch) {
-      let mouse = {
-        x: sketch.mouseX,
-        y: sketch.mouseY
-      }
+        this.createDots(this.radius)
+      },
+      createDots(radius, update=false) {
+        if (update) {
+          this.dots = this.dots.map((e, i) => {
+            return this.getDots([], radius * (0.6 ** i) )
+          })
+        } else {
+          this.dots.push([])
+          this.getDots(this.dots[this.dots.length - 1], radius)
+        }
+      },
+      getDots(array, radius) {
+        for (let i = 0; i < this.angles; i++) {
+          const angle = i * 2 * Math.PI / this.angles
+          const x = this.center.x - (radius * Math.sin(angle * -1))
+          const y = this.center.y - (radius * Math.cos(angle * -1))
+          array.push({x, y})
+        }
+        return array
+      },
+      draw (sketch) {
+        sketch.clear()
 
-      this.dots.forEach(dot => {
-        if (sketch.mouseClicked && this.isAreaDetected(mouse, dot)) {
-          let beatFound = this.beats.find(item => item.x === dot.x && item.y === dot.y)
-          if (beatFound) {
-            this.beats = this.beats.filter(beat => beat.x !== beatFound.x && beat.y !== beatFound.y)
-          } else {
-            this.beats.push({x: dot.x, y: dot.y})
+        this.drawCircles(sketch)
+        this.drawFinger(sketch)
+        this.drawDots(sketch)
+        this.drawBeats(sketch)
+      },
+      drawFinger (sketch) {
+        this.circles.forEach(radius => {
+          const end = {
+            x: this.center.x - (radius / 2 * sketch.sin(this.angle)),
+            y: this.center.y - (radius / 2 * sketch.cos(this.angle)),
           }
+          sketch.angleMode(sketch.DEGREES)
+          sketch.stroke('#6a44eb')
+          sketch.line(this.center.x, this.center.y, end.x, end.y)
+
+          this.beats.forEach(beat => {
+            if (this.isAreaDetected(end, beat)) {
+              this.drawFullDot(sketch, beat, this.beatSize * 1.6, '#d93f28')
+            }
+          })
+        })
+
+        this.angle -= this.tempo / 60
+      },
+      drawCircles (sketch) {
+        this.circles.forEach(radius => {
+          sketch.stroke('#34ebac')
+          sketch.noFill()
+          sketch.ellipse(this.width / 2, this.height / 2, radius)
+        })
+      },
+      drawDots (sketch) {
+        this.dots.forEach(group => {
+          group.forEach(item => this.drawFullDot(sketch, item, this.dotSize, '#321fb2'))
+        })
+      },
+      drawBeats (sketch) {
+        this.beats.forEach(beat => {
+          this.drawFullDot(sketch, beat, this.beatSize, '#d93f28')
+        })
+      },
+      mouseclicked ({mouseX, mouseY}) {
+        let mouse = {
+          x: mouseX,
+          y: mouseY,
         }
-      })
+
+        this.dots.forEach(dotGroup => {
+          dotGroup.forEach(dot => {
+            if (this.isAreaDetected(mouse, dot)) {
+              let beatFound = this.beats.find(item => item.x === dot.x && item.y === dot.y)
+              if (beatFound) {
+                this.beats = this.beats.filter(beat => beat.x !== beatFound.x && beat.y !== beatFound.y)
+              } else {
+                this.beats.push({x: dot.x, y: dot.y})
+              }
+            }
+          })
+        })
+      },
+      drawFullDot (sketch, dot, size, color) {
+        sketch.fill(color)
+        sketch.noStroke()
+        sketch.ellipse(dot.x, dot.y, size)
+      },
+      resetBeats () {
+        this.beats = []
+      },
+      resetCircles() {
+        this.circles = [this.circles[0]]
+        this.dots = [this.dots[0]]
+      },
+      addCircle () {
+        let radius = this.circles[this.circles.length - 1] * 0.6
+        this.circles.push(radius)
+        this.createDots(radius / 2)
+      },
     },
-    drawFullDot(sketch,dot, size, color) {
-      sketch.fill(color)
-      sketch.noStroke()
-      sketch.ellipse(dot.x, dot.y, size)
+    mounted () {
+      this.sound = this.$el.querySelector('audio')
+      window.sound = this.sound
     },
-    resetBeats() {
-      this.beats = []
+    components: {
+      VueP5,
+    },
+    watch: {
+      angles() {
+        this.createDots(this.radius, true)
+      }
     }
-  },
-  mounted() {
-    this.sound = this.$el.querySelector('audio')
-    window.sound = this.sound
-  },
-  components: {
-    VueP5
   }
-}
 </script>
